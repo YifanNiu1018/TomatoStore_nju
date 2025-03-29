c3665002419b626f2c798c09065981aca0eee592
yifan <yifanniu@smail.nju.edu.cn>
2025-03-29 20:56:41 +0800

    新增上传图片到服务器方法

---
+++ src/api/tools.ts
@@ -0,0 +1,11 @@
+import {axios} from "@/utils/request.ts";
+
+export const uploadImage = (file: File) => {
+  const formData = new FormData();
+  formData.append('multipartFile', file);
+  return axios.post('/api/images', formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data'
+    }
+  });
+};
