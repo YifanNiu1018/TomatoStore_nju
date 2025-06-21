fb3eba0b0d9768440ff3078ade25ab3b63f3c945
AAA屎山代码回收 <1422960105@qq.com>
2025-06-21 17:30:02 +0800

    新增评论功能

---
+++ src/main/java/com/example/tomatomall/vo/ForumArticleUpdateDetailVO.java
@@ -0,0 +1,13 @@
+package com.example.tomatomall.vo;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ForumArticleUpdateDetailVO {
+
+    private ForumAritcle forumAritcle;
+}
